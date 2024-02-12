cf6fe1bcaab275736d142f176b2a94a607854dad
Michal Kováčik <michal.kovacik@siemens-healthineers.com>
2024-02-12 15:00:38 +0000

    Add dall-e-3 image generator and devcontainer

--- Dockerfile
+++ Dockerfile
@@ -3,16 +3,16 @@ WORKDIR /app
 
 RUN npm install -g @angular/cli
 
-COPY ./package.json .
+COPY ./package.json ./package-lock.json ./
 RUN npm install
 
 COPY . .
 ARG API_KEY
-RUN mv src/app/app.component.ts src/app/app.component.ts.bak && \
-    apt-get update && apt-get install gettext -y && \
-    envsubst '$API_KEY' < src/app/app.component.ts.bak > src/app/app.component.ts && \
-    rm src/app/app.component.ts.bak && \
-    ng build
+RUN mv src/app/app.component.ts src/app/app.component.ts.bak
+RUN apt-get update && apt-get install gettext -y
+RUN envsubst '$API_KEY' < src/app/app.component.ts.bak > src/app/app.component.ts
+RUN rm src/app/app.component.ts.bak
+RUN npm run build
 
 FROM nginx as runtime
 COPY --from=build /app/dist/poem-generator /usr/share/nginx/html
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,39 +8,76 @@ import { Component } from '@angular/core';
 export class AppComponent {
   poem: string = "Klikni tlačidlo a ja sa inšpirujem tvojou krásou";
   poemLoaded: boolean = true;
+  imageLoading: boolean = false;
+  imageLoaded: boolean = false;
+  apiKey: string = '${API_KEY}';
+  imageUrl: string = ""
 
   getPoem() {
     this.poemLoaded = false;
-    const endpoint = 'https://api.openai.com/v1/completions';
-    const apiKey = '${API_KEY}';
+    const endpoint = 'https://api.openai.com/v1/chat/completions';
 
     const response = fetch(endpoint, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiKey}`
+        'Authorization': `Bearer ${this.apiKey}`
       },
       body: JSON.stringify({
-        prompt: 'Napíš báseň o mojej láske Janulke a naše dcére Lilianke.',
-        model: 'text-davinci-003',
-        max_tokens: 190,
+        messages: [
+          {
+            'role': 'user',
+            'content': 'Napíš báseň o mojej láske Janulke. Je to najkrajšia žena akú som kedy videl. Má ohnivo ryšavé vlasy a oči modré ako obloha. Spomeň našu dcéru, ktorá sa volá Lilianka a má 14 mesiacov. Daj dôraz na fakt, že ju milujem najviac na celom svete. Báseň sa musí rýmovať!'
+          }],
+        model: 'gpt-4',
+        max_tokens: 200,
         temperature: 0.5
       })
     }).then(response => {
       response.json().then(_ => {
-        try{
-          this.poem = this.sanitizePoem(_.choices[0].text);
+        try {
+          this.poem = this.sanitizePoem(_.choices[0].message.content);
+          this.getImage();
         }
-        catch (e){
+        catch (e) {
           this.poem = `Došlo k chybe pri skladaní básne. Umelá inteligencia môže byť práve príliš vyťažená, skús to prosím o chvíľku neskôr.`;
-          console.log(_)
+          console.log(_);
         }
         this.poemLoaded = true;
       });
     });
   }
 
+  getImage() {
+    this.imageLoading = true;
+    const endpoint = 'https://api.openai.com/v1/images/generations';
+    const response = fetch(endpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${this.apiKey}`
+      },
+      body: JSON.stringify({
+        model: 'dall-e-3',
+        prompt: "Nepíš na obrázok žiaden text a maľuj v renesančnom štýle! " + this.poem,
+        n: 1,
+        size: "1024x1024"
+      })
+    }).then(response => {
+      response.json().then(_ => {
+        try {
+          this.imageUrl = _.data[0].url;
+          this.imageLoading = false;
+          this.imageLoaded = true;
+        }
+        catch (e) {
+          console.log(_);
+        }
+      });
+    });
+  }
+
   private sanitizePoem(poem: string): string {
-    return poem.substr(0, poem.lastIndexOf(".")+1);
+    return poem.substr(0, poem.lastIndexOf(".") + 1);
   }
 }
